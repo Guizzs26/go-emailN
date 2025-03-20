b40409459ef7d2b5730966c61319525ea7dbbbd7
Guizzs26 <guiteixeira1926@gmail.com>
2025-03-20 14:53:42 -0300

    feat/test: add validations in the entity factory function to maintain domain integrity. Add unit tests for error cases when sending invalid data (empty name, content, contact)

--- internal/modules/campaign/domain/entities/campaign.go
+++ internal/modules/campaign/domain/entities/campaign.go
@@ -1,6 +1,9 @@
 package campaign
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Contact struct {
 	Email string `json:"email"`
@@ -14,10 +17,25 @@ type Campaign struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-func NewCampaign(name, content string, emails []string) *Campaign {
+func NewCampaign(name, content string, emails []string) (*Campaign, error) {
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+
+	if content == "" {
+		return nil, errors.New("content is required")
+	}
+
+	if len(emails) == 0 {
+		return nil, errors.New("at least one email is required")
+	}
 
 	contacts := make([]Contact, len(emails))
 	for i, email := range emails {
+		if email == "" {
+			return nil, errors.New("email contact cannot be empty")
+		}
+
 		contacts[i].Email = email
 	}
 
@@ -27,5 +45,5 @@ func NewCampaign(name, content string, emails []string) *Campaign {
 		Content:   content,
 		Contacts:  contacts,
 		CreatedAt: time.Now(),
-	}
+	}, nil
 }
--- internal/modules/campaign/domain/entities/campaign_test.go
+++ internal/modules/campaign/domain/entities/campaign_test.go
@@ -15,7 +15,7 @@ func TestCreateNewCampaign(t *testing.T) {
 		emails := []string{"valid@example.com", "another@example.com"}
 		now := time.Now().Add(-time.Minute)
 
-		c := NewCampaign(name, content, emails)
+		c, _ := NewCampaign(name, content, emails)
 
 		assert.Equal(name, c.Name, "Expected campaign name to be '%s', but got '%s'", name, c.Name)
 		assert.Equal(content, c.Content, "Expected campaign content to be '%s', but got '%s'", content, c.Content)
@@ -24,5 +24,55 @@ func TestCreateNewCampaign(t *testing.T) {
 		assert.Equal(emails[1], c.Contacts[1].Email, "Expected second contact email to be '%s', but got '%s'", emails[1], c.Contacts[1].Email)
 		assert.NotEmpty(c.ID, "Expected campaign ID to be generated, but got an empty value")
 		assert.Greater(c.CreatedAt, now, "Expected campaign creation time to be after '%v', but got '%v'", now, c.CreatedAt)
+	})
+}
+
+func TestCreateNewCampaignWithEmptyNameError(t *testing.T) {
+	t.Run("should return error if name is empty", func(t *testing.T) {
+		assert := assert.New(t)
+		name := ""
+		content := "Last day of discounts of up to 80%"
+		emails := []string{"valid@example.com", "another@example.com"}
+
+		_, err := NewCampaign(name, content, emails)
+
+		assert.Equal("name is required", err.Error())
+	})
+}
+
+func TestCreateNewCampaignWithEmptyContentError(t *testing.T) {
+	t.Run("should return error if content is empty", func(t *testing.T) {
+		assert := assert.New(t)
+		name := "Black Friday"
+		content := ""
+		emails := []string{"valid@example.com", "another@example.com"}
+
+		_, err := NewCampaign(name, content, emails)
+
+		assert.Equal("content is required", err.Error())
+	})
+}
+
+func TestCreateNewCampaignWithEmptyContact(t *testing.T) {
+	assert := assert.New(t)
+	name := "Black Friday"
+	content := "Last day of discounts of up to 80%"
+	emails := []string{"valid@example.com", ""}
+
+	_, err := NewCampaign(name, content, emails)
+
+	assert.Equal("email contact cannot be empty", err.Error())
+}
+
+func TestCreateNewCampaignWithZeroContacts(t *testing.T) {
+	t.Run("should return error if no emails provided", func(t *testing.T) {
+		assert := assert.New(t)
+		name := "Black Friday"
+		content := "Last day of discounts of up to 80%"
+		emails := []string{}
+
+		_, err := NewCampaign(name, content, emails)
+
+		assert.Equal("at least one email is required", err.Error())
 	})
 }
