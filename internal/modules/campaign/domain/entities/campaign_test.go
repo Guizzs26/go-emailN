712a1fde0b6f1cea9e138b84c8204df0d2567cc6
Guizzs26 <guiteixeira1926@gmail.com>
2025-03-21 12:57:51 -0300

    refac: create a TDT to test validations in a grouped way and make the code more readable

--- internal/modules/campaign/domain/entities/campaign_test.go
+++ internal/modules/campaign/domain/entities/campaign_test.go
@@ -7,126 +7,66 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateNewCampaign(t *testing.T) {
-	t.Run("should create a new campaign successfully", func(t *testing.T) {
+func TestNewCampaign_Success(t *testing.T) {
+	t.Run("should create a new valid campaign successfully", func(t *testing.T) {
 		assert := assert.New(t)
 		name := "Black Friday"
 		content := "Last day of discounts of up to 80%"
 		emails := []string{"valid@example.com", "another@example.com"}
 		now := time.Now().Add(-time.Minute)
 
-		c, _ := NewCampaign(name, content, emails)
+		campaign, err := NewCampaign(name, content, emails)
 
-		assert.Equal(name, c.Name, "Expected campaign name to be '%s', but got '%s'", name, c.Name)
-		assert.Equal(content, c.Content, "Expected campaign content to be '%s', but got '%s'", content, c.Content)
-		assert.Equal(len(c.Contacts), len(emails), "Expected %d contacts, but got %d", len(emails), len(c.Contacts))
-		assert.Equal(emails[0], c.Contacts[0].Email, "Expected first contact email to be '%s', but got '%s'", emails[0], c.Contacts[0].Email)
-		assert.Equal(emails[1], c.Contacts[1].Email, "Expected second contact email to be '%s', but got '%s'", emails[1], c.Contacts[1].Email)
-		assert.NotEmpty(c.ID, "Expected campaign ID to be generated, but got an empty value")
-		assert.Greater(c.CreatedAt, now, "Expected campaign creation time to be after '%v', but got '%v'", now, c.CreatedAt)
+		assert.NoError(err)
+		assert.Equal(name, campaign.Name, "Expected campaign name to be '%s', but got '%s'", name, campaign.Name)
+		assert.Equal(content, campaign.Content, "Expected campaign content to be '%s', but got '%s'", content, campaign.Content)
+		assert.Len(campaign.Contacts, 2, "Expected %d contacts, but got %d", len(emails), len(campaign.Contacts))
+		assert.Equal(emails[0], campaign.Contacts[0].Email, "Expected first contact email to be '%s', but got '%s'", emails[0], campaign.Contacts[0].Email)
+		assert.Equal(emails[1], campaign.Contacts[1].Email, "Expected second contact email to be '%s', but got '%s'", emails[1], campaign.Contacts[1].Email)
+		assert.NotEmpty(campaign.ID, "Expected campaign ID to be generated, but got an empty value")
+		assert.Greater(campaign.CreatedAt, now, "Expected campaign creation time to be after '%v', but got '%v'", now, campaign.CreatedAt)
 	})
 }
 
-func TestCreateNewCampaignWithEmptyNameError(t *testing.T) {
-	t.Run("should return error if name is empty", func(t *testing.T) {
-		assert := assert.New(t)
-		name := ""
-		content := "Last day of discounts of up to 80%"
-		emails := []string{"valid@example.com", "another@example.com"}
+// TDT - Table Driven Test
+func TestNewCampaign_Validation(t *testing.T) {
+	tests := []struct {
+		name         string
+		inputName    string
+		inputContent string
+		inputEmails  []string
+		expectedErr  string
+	}{
+		{"Empty name", "", "content", []string{"valid@example.com"}, "name is required"},
+		{"Empty content", "name", "", []string{"valid@example.com"}, "content is required"},
+		{"Empty email", "name", "content", []string{""}, "email contact cannot be empty"},
+		{"Empty contact (no emails)", "name", "content", []string{}, "at least one email is required"},
+		{"Invalid email", "name", "content", []string{"invalid-email@"}, "invalid email: invalid-email@"},
+		{"Empty name with only spaces", "     ", "content", []string{"valid@example.com"}, "name is required"},
+		{"Empty content with only spaces", "name", "     ", []string{"valid@example.com"}, "content is required"},
+		{"Empty email with only spaces", "name", "content", []string{"     "}, "email contact cannot be empty"},
+	}
 
-		_, err := NewCampaign(name, content, emails)
-
-		assert.Equal("name is required", err.Error())
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewCampaign(tt.inputName, tt.inputContent, tt.inputEmails)
+			assert.EqualError(t, err, tt.expectedErr)
+		})
+	}
 }
 
-func TestCreateNewCampaignWithEmptyContentError(t *testing.T) {
-	t.Run("should return error if content is empty", func(t *testing.T) {
-		assert := assert.New(t)
-		name := "Black Friday"
-		content := ""
-		emails := []string{"valid@example.com", "another@example.com"}
+/*
 
-		_, err := NewCampaign(name, content, emails)
+Note:
 
-		assert.Equal("content is required", err.Error())
-	})
-}
+[:] is used to manipulate parts of arrays or slices.
 
-func TestCreateNewCampaignWithEmptyContactError(t *testing.T) {
-	assert := assert.New(t)
-	name := "Black Friday"
-	content := "Last day of discounts of up to 80%"
-	emails := []string{"valid@example.com", ""}
+Sintax:
+[low:high]
 
-	_, err := NewCampaign(name, content, emails)
+slice[:] Copies the entire slice.
+slice[a:] From index a to the end.
+slice[:b] From start to index b (excludes).
+slice[a:b] From index a to b (excludes b).
 
-	assert.Equal("email contact cannot be empty", err.Error())
-}
-
-func TestCreateNewCampaignWithZeroContactsError(t *testing.T) {
-	t.Run("should return error if no emails provided", func(t *testing.T) {
-		assert := assert.New(t)
-		name := "Black Friday"
-		content := "Last day of discounts of up to 80%"
-		emails := []string{}
-
-		_, err := NewCampaign(name, content, emails)
-
-		assert.Equal("at least one email is required", err.Error())
-	})
-}
-
-func TestCreateNewCampaignWithValidEmailError(t *testing.T) {
-	t.Run("should return error if email is invalid", func(t *testing.T) {
-		assert := assert.New(t)
-		name := "Black Friday"
-		content := "Last day of discounts of up to 80%"
-		emails := []string{"invalid-email@"}
-
-		_, err := NewCampaign(name, content, emails)
-
-		assert.Equal("invalid email: invalid-email@", err.Error())
-	})
-}
-
-// Edge Cases
-
-func TestEmptyNameWithOnlySpacesError(t *testing.T) {
-	t.Run("", func(t *testing.T) {
-		assert := assert.New(t)
-		name := "     "
-		content := "Last day of discounts of up to 80%"
-		emails := []string{"valid@example.com", "another@example.com"}
-
-		_, err := NewCampaign(name, content, emails)
-
-		assert.Equal("name is required", err.Error())
-	})
-}
-
-func TestEmptyContentWithOnlySpacesError(t *testing.T) {
-	t.Run("", func(t *testing.T) {
-		assert := assert.New(t)
-		name := "Black Friday"
-		content := "     "
-		emails := []string{"valid@example.com", "another@example.com"}
-
-		_, err := NewCampaign(name, content, emails)
-
-		assert.Equal("content is required", err.Error())
-	})
-}
-
-func TestEmptyContactWithOnlySpacesError(t *testing.T) {
-	t.Run("", func(t *testing.T) {
-		assert := assert.New(t)
-		name := "Black Friday"
-		content := "Last day of discounts of up to 80%"
-		emails := []string{"valid@example.com", "     "}
-
-		_, err := NewCampaign(name, content, emails)
-
-		assert.Equal("email contact cannot be empty", err.Error())
-	})
-}
+*/
